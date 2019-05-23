bca301fadfe75dc80064a8e0ca821642181f1fec
Oleksandr Andrushchenko <alex.snowgirl@gmail.com>
2019-05-24 00:59:47 +0300

    +providers tables added

--- docker/mariadb/dump.sql
+++ docker/mariadb/dump.sql
@@ -1,9 +1,17 @@
 -- create database if not exists `test.keymaker`;
 
-drop table if exists `url`;
+drop table if exists `url_id_provider`;
 
-create table `url` (
-  `short` varchar(32) not null,
-  `long` varchar(2048) not null,
-  primary key (`short`)
-) engine=myisam charset=utf8;
+create table `url_id_provider` (
+  `id` int unsigned not null auto_increment,
+  `url` varchar(2048) not null,
+  primary key (`id`)
+) engine=innodb charset=utf8;
+
+drop table if exists `url_md5_provider`;
+
+create table `url_md5_provider` (
+  `md5` varchar(32) not null,
+  `url` varchar(2048) not null,
+  primary key (`md5`)
+) engine=innodb charset=utf8;
