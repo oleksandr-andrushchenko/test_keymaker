d103da769f5182e1d704bfd067ecdd3c9c0f9c75
Oleksandr Andrushchenko <alex.snowgirl@gmail.com>
2019-05-27 07:59:52 +0300

    +composer install cmd removed +up with php-consumers added

--- docker/start.sh
+++ docker/start.sh
@@ -6,6 +6,4 @@ docker-compose -p $NAME_PREFIX stop
 docker-compose -p $NAME_PREFIX rm -f
 
 docker-compose -p $NAME_PREFIX build --pull
-docker-compose -p $NAME_PREFIX up -d --force-recreate
-
-docker-compose -p $NAME_PREFIX  exec php-fpm sh -c "cd /var/www; composer install -a -o --prefer-dist"
+docker-compose -p $NAME_PREFIX up -d --force-recreate --scale php-cli-consumer=10
